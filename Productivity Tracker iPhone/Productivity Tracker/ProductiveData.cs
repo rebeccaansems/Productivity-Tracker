04e1d0b15314e064a9352908996c661178997dda
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-07-02 17:56:08 -0300

    Adapted files from Android to iOS and added SQLite
    WIP: UI (buttons, text fields, etc).

---
+++ Productivity Tracker iPhone/Productivity Tracker/ProductiveData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Productivity_Tracker
+{
+    public class ProductiveData
+    {
+        [PrimaryKey, AutoIncrement]
+        public int DataNum { get; set; }
+
+        public int DateHour { get; set; }
+        public int DateDay { get; set; }
+        public int DateMonth { get; set; }
+        public int ProdutivityLevel { get; set; }
+    }
+}
